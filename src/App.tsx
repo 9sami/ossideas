d13d69d3bea72548a0ec2e9224564b3eaf88e153
Sami <sami@devkind.com.au>
2025-06-19 11:49:32 +0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -119,12 +119,14 @@ const AppContent: React.FC = () => {
 
 function App() {
   return (
+    <AuthProvider> 
       <Router>
         <Routes>
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/*" element={<AppContent />} />
         </Routes>
       </Router>
+    </AuthProvider> 
   );
 }
 
