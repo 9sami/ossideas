df3ae3c073bfdd096b7c651c04fca5616c52abe1
Sami <sami@devkind.com.au>
2025-06-26 12:18:47 +0500

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -105,7 +105,7 @@ const Sidebar: React.FC<SidebarProps> = ({
           <div className="px-2">
             <button
               onClick={onToggle}
-              className="p-2 rounded-lg text-gray-600 hover:bg-gray-100 hover:text-orange-500 transition-colors"
+              className="p-2 ml-1 rounded-lg text-gray-600 hover:bg-gray-100 hover:text-orange-500 transition-colors"
               title={isOpen ? 'Close sidebar' : 'Open sidebar'}
             >
               {isOpen ? <X className="h-5 w-5" /> : <Menu className="h-5 w-5" />}
