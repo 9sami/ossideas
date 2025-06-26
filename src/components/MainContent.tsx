0a8b954b79e49410a43c476f84cb00753cb4afd5
Sami <sami@devkind.com.au>
2025-06-26 13:41:46 +0500

    Updated MainContent.tsx

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -152,7 +152,7 @@ const MainContent: React.FC<MainContentProps> = ({
       {/* Main Content */}
       <div className="p-6">
         {/*Built with bolt */}
-        <div className="pb-2">
+        <div className="pb-4">
           <a
             href="https://bolt.new"
             target="_blank"
