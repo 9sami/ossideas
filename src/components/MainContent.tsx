0ac14ad3332249ace97203475e455441fa776578
Sami <sami@devkind.com.au>
2025-06-26 13:40:58 +0500

    Updated MainContent.tsx

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -150,7 +150,7 @@ const MainContent: React.FC<MainContentProps> = ({
       />
 
       {/* Main Content */}
-      <div className="p-6>
+      <div className="p-6">
         {/*Built with bolt */}
         <div clas>
           <a
